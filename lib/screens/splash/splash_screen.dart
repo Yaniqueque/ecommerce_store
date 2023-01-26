015af397feac87437c5f9cd555ee622d52aa31a1
Santiago Matos <santiagomatosescobar@gmail.com>
2023-01-26 11:19:33 -0400

    SplashScreen created and implemented

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:ecommerce_store/components/components.dart';
+import 'package:flutter/material.dart';
+
+import '../../models/models.dart';
+
+class SplashScreen extends StatelessWidget {
+  static const String routeName = '/splash';
+
+  static Route route() {
+    return MaterialPageRoute(
+        settings: const RouteSettings(name: routeName),
+        builder: (_) => SplashScreen());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 2), () => Navigator.pushNamed(context, '/'));
+    return Scaffold(
+      body:
+          Column(mainAxisAlignment: MainAxisAlignment.center, children: const [
+        Center(
+          child: Image(
+            image: AssetImage('assets/images/logo.png'),
+            width: 125,
+            height: 125,
+          ),
+        )
+      ]),
+    );
+
+    //ProductCard(product: Product.products[0]),
+  }
+}
